6be83dbc1aad7bd68ec00b18870c43f87835df2b
Lynx <lynxnic99@gmail.com>
2019-02-24 20:42:48 +0100

    Fix include and other minor changes

--- read_json.cpp
+++ read_json.cpp
@@ -8,58 +8,72 @@
  * json copied from official json website at https://www.json.org/example.html
  * and is under public domain
  * json reader from Tencent/rapidjson v1.1.0 at https://github.com/Tencent/rapidjson
- * and a copy of the license can bo found at https://github.com/Tencent/rapidjson/blob/master/license.txt
+ * and a copy of the license can be found at https://github.com/Tencent/rapidjson/blob/master/license.txt
  */
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cstdio>
-#include "include/rapidjson/document.h"
-#include "include/rapidjson/writer.h"
-#include "include/rapidjson/stringbuffer.h"
 
 using namespace std;
-using namespace rapidjson;
 
 /*
  * Class that read the content of json file
  */
 class Reader {
     private:
-
+        string data;
     public:
         Reader() {
+            /*
+            {"menu": {
+              "id": "file",
+              "value": "File",
+              "popup": {
+                "menuitem": [
+                  {"value": "New", "onclick": "CreateNewDoc()"},
+                  {"value": "Open", "onclick": "OpenDoc()"},
+                  {"value": "Close", "onclick": "CloseDoc()"}
+                ]
+              }
+            }}
 
+            */
+            /*{*/
+            /*"menu": {*/
+            /*
+              "id": "file",
+              "value": "File",
+              "popup": */
+            /*
+            {
+              "menuitem": [
+                {"value": "New", "onclick": "CreateNewDoc()"},
+                {"value": "Open", "onclick": "OpenDoc()"},
+                {"value": "Close", "onclick": "CloseDoc()"}
+              ]
+            }
+            */
+        }
+
+        /* Method that saves the content of the file into the string */
+        string readFile(string filename) {
+            string data = NULL;
+            fstream json = NULL;
+            json(filename, ios::in);
+
+            json.open("myfile.txt", fstream::in);
+
+
+
+            //open file with C functions
+
+            return data;
         }
 }
 
-/*int getSize() {
-    fstream infile;
-    infile.open(filename.c_str(),ios::in);
-    infile.seekp(ios::end);
-    int file_size=infile.tellp();
-    cout<<"file size: "<<file_size<<endl;
-}*/
-
 int main(int argc, char const *argv[]) {
-    /*https://github.com/Tencent/rapidjson/blob/master/example/jsonx/jsonx.cpp*/
-    // Prepare JSON reader and input stream.
-    Reader reader;
-    char readBuffer[65536];
-    FileReadStream is(stdin, readBuffer, sizeof(readBuffer));
 
-    // Prepare JSON writer and output stream.
-    char writeBuffer[65536];
-    FileWriteStream os(stdout, writeBuffer, sizeof(writeBuffer));
-    JsonxWriter<FileWriteStream> writer(os);
-
-    // JSON reader parse from the input stream and let writer generate the output.
-    if (!reader.Parse(is, writer)) {
-        fprintf(stderr, "\nError(%u): %s\n", static_cast<unsigned>
-                (reader.GetErrorOffset()),
-                GetParseError_En(reader.GetParseErrorCode()));
-        return 1;
-    }
     return 0;
 }
